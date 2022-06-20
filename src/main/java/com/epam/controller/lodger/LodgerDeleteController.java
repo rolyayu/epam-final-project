e72c5626c237f1d6988a17511177b2050d771027
Anton Rudkovsky <ranton497@gmail.com>
2022-06-20 12:41:46 +0300

    simplefy delete algorithm

--- src/main/java/com/epam/controller/lodger/LodgerDeleteController.java
+++ src/main/java/com/epam/controller/lodger/LodgerDeleteController.java
@@ -3,13 +3,13 @@ package com.epam.controller.lodger;
 import com.epam.entity.Entity;
 import com.epam.entity.Request;
 import com.epam.entity.WorkPlan;
-import com.epam.ioc.ServiceFactory;
-import com.epam.ioc.ServiceFactoryCreator;
-import com.epam.ioc.ServiceFactoryException;
+import com.epam.factory.ServiceFactory;
+import com.epam.factory.ServiceFactoryCreator;
+import com.epam.factory.ServiceFactoryException;
 import com.epam.service.exception.ServiceException;
 import com.epam.service.exception.TransactionException;
+import org.apache.logging.log4j.LogManager;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,65 +20,50 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
-import java.util.stream.Collectors;
 
 @WebServlet("/lodger-delete")
 public class LodgerDeleteController extends HttpServlet {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Locale locale = new Locale(req.getSession().getAttribute("locale").toString());
         ResourceBundle bundle = ResourceBundle.getBundle("messages", locale);
         try (ServiceFactory factory = ServiceFactoryCreator.newInstance()) {
             String lodgerIdString = req.getParameter("lodgerId");
-            String message = null;
             if (lodgerIdString != null) {
                 try {
                     Long lodgerId = Long.valueOf(lodgerIdString);
-
                     List<Request> lodgersRequest = factory.getRequestService().getLodgerRequests(lodgerId);
-                    List<Request> sentRequests = lodgersRequest.stream()
+
+                    List<Request> processedRequests = lodgersRequest.stream()
                             .filter(Request::isInProcess)
                             .toList();
 
-                    List<Long> requestsId = lodgersRequest.stream()
-                            .map(Entity::getId)
-                            .toList();
-                    List<WorkPlan> plans = factory.getWorkPlanService().readAll()
-                            .stream()
-                            .filter(plan -> requestsId.contains(plan.getRequest().getId()))
-                            .toList();
+                    List<WorkPlan> plans = factory.getWorkPlanService().getPlansByRequests(lodgersRequest);
+
                     List<WorkPlan> processedPlans = plans.stream()
                             .filter(WorkPlan::isDone)
                             .toList();
-//                    if (lodgersRequest.isEmpty()) {
-//                        factory.getLodgerService().delete(lodgerId);
-//                    } else
-                    if (sentRequests.isEmpty() || lodgersRequest.isEmpty()) {
-                        for (Request notSentRequest : lodgersRequest) {
-                            factory.getRequestService().delete(notSentRequest.getId());
-                        }
-                        factory.getLodgerService().delete(lodgerId);
-                    } else if (!sentRequests.isEmpty() && !plans.equals(processedPlans)) {
-                        throw new TransactionException();
-                    } else if (plans.equals(processedPlans)) {
+
+                    if (processedRequests.isEmpty() || lodgersRequest.isEmpty() || plans.equals(processedPlans)) {
                         for (WorkPlan processedPlan : processedPlans) {
                             factory.getWorkPlanService().delete(processedPlan);
                         }
-                        for (Request sentRequest : sentRequests) {
+                        for (Request sentRequest : lodgersRequest) {
                             factory.getRequestService().delete(sentRequest.getId());
                         }
                         factory.getLodgerService().delete(lodgerId);
+                        LogManager.getLogger().info(bundle.getString("log4j.info.lodger.delete"));
+                        if (!req.getSession().getAttribute("currentLodger").toString().equals("epam_reviewer")) {
+                            req.getSession().setAttribute("currentLodger", null);
+                        }
+                    } else {
+                        throw new TransactionException();
                     }
                 } catch (TransactionException e) {
-                    message = bundle.getString("error.lodger.delete.request.sent");
+                    LogManager.getLogger().error(bundle.getString("log4j.error.lodger.delete"));
                 }
             }
-            if (message != null) {
-                System.out.println(message);
-                resp.sendRedirect("lodger-view?message=" + URLDecoder.decode(message, StandardCharsets.UTF_8));
-            } else {
-                resp.sendRedirect("lodger-view");
-            }
+            resp.sendRedirect("lodger-view");
         } catch (ServiceException | ServiceFactoryException e) {
             e.printStackTrace();
         }
