16e46d49443b0c4406ca3c6d45aaa511c8c51935
Anton Rudkovsky <ranton497@gmail.com>
2022-06-20 12:38:55 +0300

    let it be

---
+++ src/main/java/com/epam/controller/lodger/LodgerAddController.java
@@ -0,0 +1,16 @@
+package com.epam.controller.lodger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/lodger-add")
+public class LodgerAddController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/jsp/lodger/lodger-add.jsp").forward(req,resp);
+    }
+}
