4766865be008bc593ba4361267ddaa9a6a4a4e2c
Anton Rudkovsky <ranton497@gmail.com>
2022-06-20 12:40:13 +0300

    cosmetic changes

--- src/main/java/com/epam/controller/lodger/LodgerViewServlet.java
+++ src/main/java/com/epam/controller/lodger/LodgerViewServlet.java
@@ -1,18 +1,19 @@
 package com.epam.controller.lodger;
 
 import com.epam.entity.Lodger;
-import com.epam.ioc.ServiceFactory;
-import com.epam.ioc.ServiceFactoryCreator;
-import com.epam.ioc.ServiceFactoryException;
+import com.epam.factory.ServiceFactory;
+import com.epam.factory.ServiceFactoryCreator;
+import com.epam.factory.ServiceFactoryException;
 import com.epam.service.exception.ServiceException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
+@WebServlet("/lodger-view")
 public class LodgerViewServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -20,10 +21,8 @@ public class LodgerViewServlet extends HttpServlet {
             List<Lodger> lodgers = serviceFactory.getLodgerService().findAll();
             req.setAttribute("lodgers", lodgers);
             req.getRequestDispatcher("/WEB-INF/jsp/lodger/lodger-view.jsp").forward(req, resp);
-        } catch (ServletException | ServiceFactoryException | ServiceException e) {
+        } catch (ServiceFactoryException | ServiceException e) {
             e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
     }
 }
