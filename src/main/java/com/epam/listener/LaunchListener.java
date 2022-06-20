c9be64fcfec3b3e55bfe1696c586909e12068cf7
Anton Rudkovsky <ranton497@gmail.com>
2022-06-20 12:37:44 +0300

    added logging

--- src/main/java/com/epam/listener/LaunchListener.java
+++ src/main/java/com/epam/listener/LaunchListener.java
@@ -1,14 +1,17 @@
 package com.epam.listener;
 
 import com.epam.pool.ConnectionPool;
+import org.apache.logging.log4j.LogManager;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.util.ResourceBundle;
 
 public class LaunchListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        ResourceBundle bundle = ResourceBundle.getBundle("messages");
         ServletContext context = sce.getServletContext();
         String jdbcDriver = context.getInitParameter("jdbc-driver");
         String jdbcUrl = context.getInitParameter("jdbc-url");
@@ -19,8 +22,9 @@ public class LaunchListener implements ServletContextListener {
         try {
             ConnectionPool.init(jdbcDriver,jdbcUrl,jdbcUser,
                     jdbcPassword,validationTime,maxConnections);
+            LogManager.getLogger().info(bundle.getString("log4j.info.pool.init"));
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            LogManager.getLogger().info(bundle.getString("log4j.error.pool.init"));
         }
     }
 
