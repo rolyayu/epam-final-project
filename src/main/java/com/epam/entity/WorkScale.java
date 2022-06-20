95c1e293168864f3257c50d2f840390893f64e19
Anton Rudkovsky <ranton497@gmail.com>
2022-06-20 12:37:04 +0300

    new feature

--- src/main/java/com/epam/entity/WorkScale.java
+++ src/main/java/com/epam/entity/WorkScale.java
@@ -1,14 +1,31 @@
 package com.epam.entity;
 
 public enum WorkScale {
-    SMALL("SMALL"),
-    MEDIUM("MEDIUM"),
-    LARGE("LARGE");
+    SMALL("SMALL") {
+        @Override
+        public int getNeededWorkers() {
+            return 2;
+        }
+    },
+    MEDIUM("MEDIUM") {
+        @Override
+        public int getNeededWorkers() {
+            return 3;
+        }
+    },
+    LARGE("LARGE") {
+        @Override
+        public int getNeededWorkers() {
+            return 4;
+        }
+    };
     private final String scale;
 
     public String getScale() {
         return scale;
     }
+
+    public abstract int getNeededWorkers();
 
     WorkScale(String scale) {
         this.scale = scale;
